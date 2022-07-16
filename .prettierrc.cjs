81699f6a1abdb108f05b5ee0f6f5104e8fe6e7bc
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-16 15:59:31 +0700

    feat: install deps, and config dev environment

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  importOrder: ['^@/styles/(.*)$', '^@/components(.*)$', '^@/(.*)$', '^[./]', '^'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
+}
