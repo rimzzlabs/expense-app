4d09c9ffc4c26d30f84c99324a2ce62665c4bddd
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-16 21:57:04 +0700

    feat: initialize jotai store

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,11 @@
-import { Theme } from 'expense-app'
+import { Expense, ExpenseHistory, Theme, User } from 'expense-app'
+import { atom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 
 export const themeAtom = atomWithStorage<Theme>('APP_THEME', 'light')
+
+export const userAtom = atom<User | null>(null)
+
+export const expenseListsAtom = atom<Expense[]>([])
+
+export const historyListsAtom = atom<ExpenseHistory[]>([])
