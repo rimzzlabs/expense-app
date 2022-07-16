638e9b814a7aea6d6e38128550947b52aad11b1c
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-16 16:12:07 +0700

    feat: update command script

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,8 +1,14 @@
 const path = require('path')
 
 const buildEslintCommand = (filenames) =>
-  `yarn lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+  `yarn lint --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+
+const formatFile = (filenames) =>
+  `yarn format --write --ignore-unkown ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand]
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, formatFile],
+  './src/**/*.{md,css,json}': [formatFile]
 }
