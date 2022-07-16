2d9ef449f5cf19ab39625ab0e1762c7997729231
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-17 00:55:46 +0700

    feat: remove side effect

--- src/components/AuthLayer.tsx
+++ src/components/AuthLayer.tsx
@@ -2,23 +2,12 @@ import { LoadingPage } from '@/components/UI'
 
 import { useUser } from '@/hooks'
 
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-
 type AuthLayerProps = {
   children: React.ReactNode
 }
 
 const AuthLayer: React.FunctionComponent<AuthLayerProps> = ({ children }) => {
   const { user } = useUser()
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    console.log('run')
-    if (!user) {
-      navigate('/signin', { replace: true })
-    }
-  }, [user])
 
   if (!user) {
     return <LoadingPage />
