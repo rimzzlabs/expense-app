e40cebea63b29ae4fa8ed5349c8f882f7923f5da
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-16 22:50:59 +0700

    feat: create signin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import { Header } from '@/components/UI'
+import { Header, LoadingPage } from '@/components/UI'
 
+import { useTheme } from '@/hooks'
 import { Layout } from '@/templates'
-
-import { useTheme } from './hooks'
 
 import { Suspense, lazy } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 const Home = lazy(() => import('./pages').then((m) => ({ default: m.Home })))
 const Signup = lazy(() => import('./pages').then((m) => ({ default: m.Signup })))
+const Signin = lazy(() => import('./pages').then((m) => ({ default: m.Signin })))
 
 const NotFound = lazy(() => import('./pages').then((m) => ({ default: m.NotFound })))
 
 const ExpenseApp: React.FunctionComponent = () => {
   useTheme()
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoadingPage />}>
       <Header />
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='signup' element={<Signup />} />
+          <Route path='signin' element={<Signin />} />
         </Route>
 
         <Route path='/404' element={<NotFound />} />
