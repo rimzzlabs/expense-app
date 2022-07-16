52de22e3752b88e505e3f0a87829daf291b77a12
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-16 20:52:35 +0700

    feat: infer type for environment variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_SUPABASE_ANON_KEY: string
+  readonly VITE_APP_SUPABASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
