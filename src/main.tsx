4f0d039a8c96593d50156fe5e8e085037b5e6178
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-16 16:27:38 +0700

    feat: src directory init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './styles/tailwind.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import { createRoot } from 'react-dom/client'
+
+const root = document.getElementById('root') as HTMLElement
+
+createRoot(root).render(<App />)
