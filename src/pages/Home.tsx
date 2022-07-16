66f393c77c25a004cad361850cb1843087e9ac07
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-17 00:58:27 +0700

    feat: add todos tips on HomePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,39 @@
 import { AuthLayer } from '@/components'
 
+import { useUser } from '@/hooks'
+
+import { useId } from 'react'
+import { Navigate } from 'react-router-dom'
+
 const HomePage: React.FunctionComponent = () => {
+  const todolists = [
+    'Create new expense',
+    'Create new income',
+    'Create new outcome',
+    'Calculate expense'
+  ]
+
+  const { user } = useUser()
+  if (!user) {
+    return <Navigate to='/signin' replace />
+  }
+
   return (
     <AuthLayer>
-      <h1>Helllo World</h1>
+      <section>
+        <h1>Welcome back, {user.username}!</h1>
+        <p className='mt-4 mb-2'>Here&apos;s what you might want to do here</p>
+        <ul className='list-disc list-inside'>
+          {todolists.map((todo) => {
+            const id = useId()
+            return (
+              <li className='[&::marker]:text-primary-5' key={id}>
+                {todo}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
     </AuthLayer>
   )
 }
