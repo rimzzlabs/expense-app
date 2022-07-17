48435edc4a8b4323f38d20b58b1ea2af1f4c1bdf
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-17 17:33:19 +0700

    feat: add expense list prototype component of Expense

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
-import { AuthLayer } from '@/components'
+import { AuthLayer, ButtonLink, ErrorButton } from '@/components'
 
 import { useExpense, useUser } from '@/hooks'
+import { formatCurrency, formatDate, twclsx } from '@/utils'
 
+import { HiArrowSmRight, HiTrash } from 'react-icons/hi'
 import { Navigate } from 'react-router-dom'
 
 const HomePage: React.FunctionComponent = () => {
@@ -23,14 +25,55 @@ const HomePage: React.FunctionComponent = () => {
 
       <section className='py-10'>
         <h2 className='mb-2.5'>💰My Expenses</h2>
-        <p>List of your expenses</p>
+        <p className='mb-4'>List of your expenses, to view all your expense, go to expense page</p>
 
         {expenseLists.length > 0 ? (
-          <div>
-            <p>H</p>
+          <div className={twclsx('grid grid-cols-1 gap-4', 'w-full flex-auto')}>
+            {expenseLists.map((expense) => (
+              <div
+                className={twclsx(
+                  'grid grid-cols-2 gap-2',
+                  'w-full p-4 rounded-lg shadow-md dark:shadow-none',
+                  'bg-theme-1 dark:bg-theme-7'
+                )}
+                key={expense.id}
+              >
+                <div className='w-full'>
+                  <h3 className='mb-2'>{expense.title}</h3>
+                  <p className='mb-4'>{formatDate(expense.created_at)}</p>
+                </div>
+
+                <span className='w-full text-xl md:text-4xl font-bold text-right text-success-1'>
+                  {formatCurrency(expense.total_money)}
+                </span>
+
+                <div
+                  className={twclsx(
+                    'inline-flex items-center justify-end',
+                    'gap-4 w-full col-span-2'
+                  )}
+                >
+                  <ButtonLink
+                    to={'/expense/' + expense.id}
+                    className={twclsx(
+                      'inline-flex items-center gap-1',
+                      'py-1.5 px-4 md:py-2.5 md:px-6'
+                    )}
+                  >
+                    <span>View</span>
+                    <HiArrowSmRight />
+                  </ButtonLink>
+
+                  <ErrorButton className='p-2 md:p-3'>
+                    <span className='sr-only'>delete expense</span>
+                    <HiTrash />
+                  </ErrorButton>
+                </div>
+              </div>
+            ))}
           </div>
         ) : (
-          <p>You don&apos;t have any expense right not, add some?</p>
+          <p>You don&apos;t have any expense right now, add some?</p>
         )}
       </section>
     </AuthLayer>
