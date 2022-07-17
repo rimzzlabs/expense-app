ce139c8ad2f8a5d4f547676bf5ecbc1766c05e76
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-17 17:22:01 +0700

    feat: create schema for adding a new expense

--- src/utils/yupSchema.ts
+++ src/utils/yupSchema.ts
@@ -16,3 +16,18 @@ export const signinSchema = yup.object().shape({
   email: yup.string().required(requiredMsg).email('Invalid email address'),
   password: yup.string().required(requiredMsg)
 })
+
+export const createExpenseSchema = yup.object().shape({
+  title: yup
+    .string()
+    .required(requiredMsg)
+    .min(6, 'At least 6 characters')
+    .max(16, 'Should less than 16 characters'),
+  total_money: yup
+    .number()
+    .transform(() => undefined)
+    .nullable()
+    .notRequired()
+    .required(requiredMsg)
+    .lessThan(10000000000, 'maximum money exceeded')
+})
