076380924f2184e03646e0707e6898d34e7ed071
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-17 13:21:24 +0700

    feat: try to implement useExpense

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,37 @@
 import { AuthLayer } from '@/components'
 
-import { useUser } from '@/hooks'
+import { useExpense, useUser } from '@/hooks'
 
-import { useId } from 'react'
 import { Navigate } from 'react-router-dom'
 
 const HomePage: React.FunctionComponent = () => {
-  const todolists = [
-    'Create new expense',
-    'Create new income',
-    'Create new outcome',
-    'Calculate expense'
-  ]
-
   const { user } = useUser()
+  const { expenseLists } = useExpense()
+
   if (!user) {
     return <Navigate to='/signin' replace />
   }
 
   return (
     <AuthLayer>
-      <section>
+      <section className='pt-10'>
         <h1>Welcome back, {user.username}!</h1>
-        <p className='mt-4 mb-2'>Here&apos;s what you might want to do here</p>
-        <ul className='list-disc list-inside'>
-          {todolists.map((todo) => {
-            const id = useId()
-            return (
-              <li className='[&::marker]:text-primary-5' key={id}>
-                {todo}
-              </li>
-            )
-          })}
-        </ul>
+        <p className='mt-4 max-w-prose'>
+          ExpenseApp can help you notes your income, outcome and manage your expense more easy.
+        </p>
+      </section>
+
+      <section className='py-10'>
+        <h2 className='mb-2.5'>💰My Expenses</h2>
+        <p>List of your expenses</p>
+
+        {expenseLists.length > 0 ? (
+          <div>
+            <p>H</p>
+          </div>
+        ) : (
+          <p>You don&apos;t have any expense right not, add some?</p>
+        )}
       </section>
     </AuthLayer>
   )
