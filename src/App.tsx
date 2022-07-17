13d6a54bddd9fe4b217d5994e11a05a85e8a06b9
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-17 13:26:41 +0700

    feat: modify Dialog prompt strategy and implement usePrompt on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header, LoadingPage } from '@/components/UI'
 
-import { useTheme } from '@/hooks'
+import { usePrompt, useTheme } from '@/hooks'
 import { Layout } from '@/templates'
 
 import { Suspense, lazy } from 'react'
+import { Toaster } from 'react-hot-toast'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import colors from 'tailwindcss/colors'
 
 const Home = lazy(() => import('./pages').then((m) => ({ default: m.Home })))
 const Signup = lazy(() => import('./pages').then((m) => ({ default: m.Signup })))
@@ -13,10 +15,22 @@ const Profile = lazy(() => import('./pages').then((m) => ({ default: m.Profile }
 
 const NotFound = lazy(() => import('./pages').then((m) => ({ default: m.NotFound })))
 
+const ModalPrompt = lazy(() => import('@/components').then((m) => ({ default: m.ModalPrompt })))
+
 const ExpenseApp: React.FunctionComponent = () => {
-  useTheme()
+  const { theme } = useTheme()
+  const { state, closePrompt } = usePrompt()
+
   return (
     <Suspense fallback={<LoadingPage />}>
+      <Toaster
+        toastOptions={{
+          style: {
+            backgroundColor: theme === 'dark' ? colors.neutral[800] : colors.white,
+            color: theme === 'dark' ? colors.white : colors.neutral[900]
+          }
+        }}
+      />
       <Header />
       <Routes>
         <Route path='/' element={<Layout />}>
@@ -30,6 +44,16 @@ const ExpenseApp: React.FunctionComponent = () => {
 
         <Route path='*' element={<Navigate to='/404' />} />
       </Routes>
+
+      <Suspense fallback={<LoadingPage />}>
+        <ModalPrompt
+          title={state.title}
+          message={state.message}
+          onClose={closePrompt}
+          onConfirm={state.onConfirm}
+          show={state.isOpen}
+        />
+      </Suspense>
     </Suspense>
   )
 }
