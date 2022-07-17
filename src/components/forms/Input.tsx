960dca474f3f663e1811dbaee1ac58def6164a2e
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-17 17:29:40 +0700

    style: smaller placheolder on smaller device

--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -14,7 +14,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
       'focus:border-primary-5',
       'focus:ring-primary-5',
       'bg-theme-2 dark:bg-theme-7 border-theme-4 dark:border-theme-6',
-      'placeholder:text-theme-6 dark:placeholder:text-theme-4',
+      'placeholder:text-theme-6 dark:placeholder:text-theme-4 placeholder:text-sm md:placeholder:text-base',
       c
     )
     return createElement('input', { ...props, ref, autoComplete: 'off', className, type })
