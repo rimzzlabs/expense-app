9c236836040ad800e44836f56e799f108efebe4c
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-17 13:18:52 +0700

    style: re export Button and add ring color to primary color

--- src/components/UI/Buttons/Primary.tsx
+++ src/components/UI/Buttons/Primary.tsx
@@ -10,7 +10,11 @@ type PrimaryButtonProps = React.DetailedHTMLProps<
 const Primary: React.FunctionComponent<PrimaryButtonProps> = (props) => (
   <Button
     {...props}
-    className={twclsx('bg-primary-5 text-theme-1', 'hover:bg-primary-6', props.className)}
+    className={twclsx(
+      'border-primary-5 dark:border-primary-5 bg-primary-5 text-theme-1',
+      'hover:bg-primary-6',
+      props.className
+    )}
   >
     {props.children}
   </Button>
--- src/components/UI/Buttons/index.ts
+++ src/components/UI/Buttons/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button'
 export { default as PrimaryButton } from './Primary'
+export { default as ErrorButton } from './Error'
 export { default as ButtonLink } from './ButtonLink'
