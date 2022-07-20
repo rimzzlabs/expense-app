4ebf131840c9fde79d595c0ae88402d703eba66d
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-20 23:25:40 +0700

    feat: add more function and data manipulation on useExpenseHistory

--- src/hooks/useExpenseHistory.tsx
+++ src/hooks/useExpenseHistory.tsx
@@ -1,36 +1,49 @@
-import { getExpenseHistory } from '@/services'
+import { createExpenseHistory, getExpenseHistory } from '@/services'
 import * as atoms from '@/store'
 
+import { CreateHistoryPayload } from 'expense-app'
 import { useAtom } from 'jotai'
 import { useCallback, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
 
 const useExpenseHistory = () => {
   const [expenseHistory, setExpenseHistory] = useAtom(atoms.historyListsAtom)
+  const [expenseDetail] = useAtom(atoms.expenseAtom)
   const [isOpen, setIsOpen] = useAtom(atoms.createdHistoryModal)
-  const param = useParams()
 
   const openModal = useCallback(() => setIsOpen(true), [])
   const closeModal = useCallback(() => setIsOpen(false), [])
 
-  const refreshExpenseHistory = useCallback(
-    async () => await getExpenseHistory(param.id as string),
-    []
+  const refreshExpenseHistory = useCallback(async () => {
+    const response = await getExpenseHistory(expenseDetail?.history_id as string)
+
+    if (!response) return
+    setExpenseHistory(response)
+  }, [expenseDetail?.history_id, expenseHistory])
+
+  const addHistory = useCallback(
+    async (payload: CreateHistoryPayload) =>
+      await createExpenseHistory(payload, expenseDetail?.history_id as string),
+
+    [expenseDetail?.history_id, expenseHistory]
   )
 
   useEffect(() => {
-    if (param.id) {
+    if (expenseDetail?.history_id) {
       ;(async () => {
-        const response = await refreshExpenseHistory()
-        if (response) setExpenseHistory(response)
+        if (!expenseDetail.history_id) return
+
+        await refreshExpenseHistory()
       })()
     }
-  }, [])
+  }, [expenseDetail?.history_id])
+
   return {
     expenseHistory,
     isOpen,
     openModal,
-    closeModal
+    closeModal,
+    addHistory,
+    refreshExpenseHistory
   }
 }
 
