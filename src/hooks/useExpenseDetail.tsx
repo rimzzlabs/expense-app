fec7d47dee480ea643fa963d3db8f0e317b1164c
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-23 00:17:02 +0700

    feat: create expenseDetail hooks

---
+++ src/hooks/useExpenseDetail.tsx
@@ -0,0 +1,37 @@
+import * as atoms from '@/store'
+
+import { useAtom } from 'jotai'
+import { useCallback } from 'react'
+import { useParams } from 'react-router-dom'
+
+const useExpenseDetail = () => {
+  const param = useParams()
+  const [expenseDetail, setExpenseDetail] = useAtom(atoms.expenseAtom)
+  const [expenseLists] = useAtom(atoms.expenseListsAtom)
+  const [expenseHistory] = useAtom(atoms.historyListsAtom)
+
+  const refreshExpenseDetail = useCallback(() => {
+    const filteredExpense = expenseLists.filter((e) => e.history_id === param.id)[0]
+
+    const totalIncome = expenseHistory
+      ?.filter((e) => e.type === 'income')
+      .reduce((acc, cur) => acc + cur.amount, 0)
+    const totalOutcome = expenseHistory
+      ?.filter((e) => e.type === 'outcome')
+      .reduce((acc, cur) => acc + cur.amount, 0)
+
+    setExpenseDetail({
+      ...filteredExpense,
+      totalIncome,
+      totalOutcome,
+      currentMoney: filteredExpense.total_money + totalIncome - totalOutcome
+    })
+  }, [expenseLists, expenseHistory])
+
+  return {
+    expenseDetail,
+    refreshExpenseDetail
+  }
+}
+
+export default useExpenseDetail
