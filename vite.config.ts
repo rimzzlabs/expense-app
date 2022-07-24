cd49e0bfc95c31ff72c74f744714c7d5e832c0fa
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-24 18:40:55 +0700

    feat: disable source map file

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
   build: {
     minify: 'esbuild',
     cssCodeSplit: true,
-    sourcemap: 'hidden',
     polyfillModulePreload: true,
     reportCompressedSize: true
   }
