9b541d273f27076359540d679a48b90e3fa0491f
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-30 15:51:59 +0700

    feat: applying on page

--- src/store/data.ts
+++ src/store/data.ts
@@ -6,7 +6,10 @@ export const userAtom = atom<User | null>(null)
 export const expenseDetailAtom = atom<ExpenseDetail | null>(null)
 export const expenseListsAtom = atom<Expense[]>([])
 export const filteredExpenseListsAtom = atom<Expense[]>([])
+export const queryExpenseListsAtom = atom('')
 
 export const historyListsAtom = atom<ExpenseHistory[]>([])
+export const filteredHistoryListsAtom = atom<ExpenseHistory[]>([])
+export const queryHistoryListsAtom = atom('')
 
 export const hashQuery = atom<HashQuery>({})
