fc7a9f116c5cde4960fe8da47e9eb35f5172a9fb
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-30 15:49:27 +0700

    feat: reusable Searchbar Component

---
+++ src/components/UI/Searchbar.tsx
@@ -0,0 +1,27 @@
+import { Input } from '@/components'
+
+import { HiOutlineSearch } from 'react-icons/hi'
+
+type SearchbarProps = {
+  value: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  placeholder?: string
+}
+
+const Searchbar: React.FunctionComponent<SearchbarProps> = (props) => (
+  <div className='my-4 md:my-8'>
+    <div className='relative'>
+      <Input
+        type='text'
+        className='peer w-full md:h-12 pr-8 border-none focus:ring-0'
+        placeholder={props.placeholder ?? 'Search..'}
+        value={props.value}
+        onChange={props.onChange}
+      />
+
+      <HiOutlineSearch className='absolute right-1 top-1/2 -translate-y-1/2 flex items-center justify-center w-8 opacity-30 peer-focus:opacity-100' />
+    </div>
+  </div>
+)
+
+export default Searchbar
